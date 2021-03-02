c63b31c6d56f93c1861d80dac54ac5dc14436d2b
Rando T <rt.peakonto@gmail.com>
2021-03-02 16:40:11 +0200

    Ei tea ma mis ma teen.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Katsetused Concuri moodi.
+
+Pooleli kood. Ei tööta. Ära raiska sellele oma aega.
--- src/Maikuu3.purs
+++ src/Maikuu3.purs
@@ -6,10 +6,10 @@ import Data.Maybe
 import Data.Tuple
 import Prelude
 
-import Data.Array (mapWithIndex, updateAt, (!!))
-import Data.Array.ST.Iterator (next)
+import Data.Array (mapWithIndex, updateAt, (!!), foldl)
+import Data.Array as Array
 import Data.Either (Either(..))
-import Data.List (List(..), foldl, (:))
+import Data.List (List(..), (:))
 import Data.Traversable (for, for_, sequence)
 import Effect (Effect)
 import Effect (Effect)
@@ -28,12 +28,14 @@ newtype Place = Place {loc::List Int, document::Document}
 
 newtype Msg = Msg {event::Event, revloc:: List Int}
 
-data Widget r = Widget (Effect ({info :: Maybe r, put :: Put r}))
 
-type Exhibit r = {nodes :: Array Node, next :: Msg -> Widget r}
+data Widget r = Widget (Effect (WidgetResult r))
+
+type WidgetResult r = {info :: Maybe r, put :: Put r}
 
 newtype Put r = Put (Place -> Effect (Exhibit r))
 
+type Exhibit r = {nodes :: Array Node, next :: Msg -> Widget r}
 
 
 
@@ -66,6 +68,10 @@ instance bindGen :: Bind Widget where
 
 instance mGen :: Monad (Widget)
 
+instance monadEffectWidget :: MonadEffect Widget where
+    liftEffect e = Widget do
+        v <- e 
+        pure $  {info: Just v, put:Put \place -> pure {nodes: [], next: \msg -> pure v}}
 
 runAll :: forall r. Array (Widget r) -> Effect (Array ({info:: Maybe r, put:: Put r}))
 runAll widgets = for widgets \(Widget eff) -> eff
@@ -74,38 +80,120 @@ placeAll :: forall r. Place -> Array (Put r) ->Effect (Array (Exhibit r))
 placeAll (Place {loc, document}) puts = 
     sequence $ mapWithIndex (\i (Put put) -> put (Place {loc:i:loc, document})) puts
 
-and :: forall o r. Array (Widget r) -> Widget (Array ({info::r, put::Put r}))
-and widgets = Widget $ do
-    rwgs  <- runAll widgets
-    let resultInfo = for rwgs \{info, put} -> case info of
-            Just i ->  Just {info: i, put:put}
-            Nothing -> Nothing
-    let puts = map (\{info, put: put} -> put) rwgs
-    let firstResult = pure $ {
+--makeWidgetResult :: forall i r. Maybe i -> (Array (WidgetResult r) -> WidgetResult i) -> WidgetResult i 
+makeWidgetResult :: forall r1 r2.
+  Maybe r1 -> (Array (WidgetResult r2) -> WidgetResult r1) -> Array (WidgetResult r2) -> WidgetResult r1
+makeWidgetResult resultInfo helper rwgs =
+    let firstResult = {
         info: resultInfo,
         put: Put \place -> do
+            let puts = map (\{info, put: put} -> put) rwgs
             exhibits <- placeAll place puts
-            pure {
-                nodes: join $ map (\{nodes, next} -> nodes) exhibits,
-                next: \msg -> Widget $ do
+            let Place {loc} = place
+            let next =  \msg -> Widget $ do
                     let Msg {event, revloc} = msg
                     case revloc of
                         index:more -> case exhibits!!index of
                             Just {nodes, next} -> do
                                 let Widget newWidgetEffect = next $ Msg {event, revloc:more}
-                                {info, put} <- newWidgetEffect
-
-                                ?k
+                                newWidgetResult <- newWidgetEffect
+                                let newRwgs = fromMaybe rwgs $ Array.updateAt index newWidgetResult rwgs
+                                pure $ helper newRwgs
+                                
                             Nothing -> do
-                                error $ "Widget and at " <> show loc <> 
+                                error $ "array widget at " <> show loc <> 
                                     " recieived invalid index" <> show index <> 
                                     " : " <> show more <> "."
-                                firstResult
+                                pure $ firstResult
                         Nil -> do
-                            error $ "Widget and at " <> show loc <> 
+                            error $ "array widget at " <> show loc <> 
                                     " recieived event."
-                            firstResult
+                            pure $ firstResult
+            pure {
+                nodes: join $ map (_.nodes) exhibits,
+                next
             }
     }
-    firstResult
+    in firstResult
+
+andHelper :: forall r. Array (WidgetResult r) -> WidgetResult (Array {info::r, widget::Widget r})
+andHelper rwgs =
+    let 
+        resultInfo :: Maybe (Array {info:: r, widget:: Widget r})
+        resultInfo = for rwgs \{info, put} -> case info of
+            Just i ->  Just {info: i, widget: Widget $ pure $ {info, put}}
+            Nothing -> Nothing
+        
+    in makeWidgetResult resultInfo andHelper rwgs
+
+and :: forall r. Array (Widget r) -> Widget (Array {info::r, widget::Widget r})
+and widgets = Widget do
+    rwgs  <- runAll widgets
+    pure $ andHelper rwgs
+
+
+orHelper :: forall r. Array (WidgetResult r) -> WidgetResult {info::r, widgets:: Array (Widget r)}
+orHelper rwgs =
+    let 
+        resultInfo :: Maybe {info::r, widgets:: Array (Widget r)}
+        resultInfo = foldl f Nothing rwgs
+            where
+                f (Just x) e = Just x
+                f Nothing {info: Just i} = Just {info: i, widgets: map (\wr -> Widget $ pure wr) rwgs}
+                f Nothing {info: Nothing} = Nothing
+
+    in makeWidgetResult resultInfo orHelper rwgs
+
+or :: forall r. Array (Widget r) -> Widget {info::r, widgets:: Array (Widget r)}
+or widgets = Widget do
+    rwgs  <- runAll widgets
+    pure $ orHelper rwgs
+
+text :: forall a. String -> Widget a
+text t = Widget $ pure {
+    info: Nothing,
+    put: Put $ \(Place {loc, document}) -> do
+        this <- Text.toNode <$> createTextNode t document
+        pure $ {
+            nodes: [this],
+            next: \msg -> do
+                liftEffect $ error $ "Text widget" <> show t <> " at " <> show loc <> " got a message."
+                text t
+        }
+}
     
+
+    
+el :: forall r. String -> Maybe r -> Widget r -> Widget r
+el tag onclick (Widget widgetEffect) = Widget do
+    {info, put: (Put childPut)} <- widgetEffect
+    let fin = {
+        info,
+        put: Put \(Place {loc, document}) -> do
+            {nodes, next} <- childPut $ Place {loc: 0:loc, document} 
+            this <- createElement tag document
+            let thisNode = Element.toNode this
+            for_ nodes \childNode -> appendChild childNode thisNode
+            pure {
+                nodes: [thisNode],
+                next: \(Msg {event, revloc}) -> Widget
+                    case revloc of
+                        0:more ->  do
+                            let newWidget = next $ Msg {event, revloc:more}
+                            let Widget newef = el tag onclick newWidget
+                            newef
+                        index:more -> do
+                            error $ "element widget at " <> show loc <> 
+                                    " recieived invalid index" <> show index <> 
+                                    " : " <> show more <> "."
+                            pure $ fin
+                        Nil -> do
+                            
+                            error $ "array widget at " <> show loc <> 
+                                    " recieived event."
+                            pure $ fin
+                    
+            }
+    }
+    pure fin    
+ 
